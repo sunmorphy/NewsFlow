b0268711aae25264bfb32ec347cc00293ba5b534
Andikas <Andpuji27@gmail.com>
2025-12-09 06:15:33 +0700

    [ADD] Repository

---
+++ lib/data/repository/news_repository.dart
@@ -0,0 +1,42 @@
+import '../local/database_helper.dart';
+import '../local/models/local_article.dart';
+import '../remote/news_service.dart';
+
+class NewsRepository {
+  final NewsService _apiService;
+  final DatabaseHelper _dbHelper;
+
+  NewsRepository({NewsService? apiService, DatabaseHelper? dbHelper})
+    : _apiService = apiService ?? NewsService(),
+      _dbHelper = dbHelper ?? DatabaseHelper.instance;
+
+  Future<List<LocalArticle>> getNews({String category = 'Latest'}) async {
+    try {
+      final remoteArticles = await _apiService.getTopHeadlines(
+        category: category,
+      );
+
+      final localArticles = remoteArticles.map((remote) {
+        return LocalArticle(
+          title: remote.title ?? 'No Title',
+          author: remote.source?.name ?? remote.author ?? 'Unknown',
+          description: remote.description ?? '',
+          urlToImage: remote.urlToImage ?? '',
+          publishedAt: remote.publishedAt ?? '',
+          content: remote.content ?? '',
+          category: category,
+        );
+      }).toList();
+
+      if (localArticles.isNotEmpty) {
+        await _dbHelper.cacheArticles(localArticles);
+      }
+
+      return localArticles;
+    } catch (e) {
+      final cachedNews = await _dbHelper.getCachedArticles(category: category);
+
+      return cachedNews;
+    }
+  }
+}
